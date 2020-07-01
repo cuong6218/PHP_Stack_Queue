c7e3bb15d925c356e7cf27992aa8185a8c5c2b63
cuong6218 <cuong6218@domain.com>
2020-07-02 01:49:49 +0700

    add file hang doi kham benh

---
+++ PreMedic/Patient.php
@@ -0,0 +1,16 @@
+<?php
+    class Patient{
+        public $name;
+        public $code;
+        function __construct($name, $code)
+        {
+            $this->name = $name;
+            $this->code = $code;
+        }
+        function showInfo(){
+            return $this->name.$this->code;
+        }
+        // function showCode(){
+        //     return $this->code;
+        // }
+    }
---
+++ PreMedic/PreExam.php
@@ -0,0 +1,50 @@
+<?php
+include_once('Patient.php');
+class PreExam
+{
+    private $pre;
+    private $count;
+    function __construct()
+    {
+        $this->pre = array();
+        $this->count = 0;
+    }
+    function addPatient($name, $code)
+    {
+        $patient = new Patient($name, $code);
+        array_push($this->pre, $patient->name.' - '.$patient->code);
+        $this->count++;
+    }
+    function curePatient()
+    {
+        $patient = $this->pre[0];
+        array_shift($this->pre);
+        echo 'this patient with info ' . $patient->showInfo() . ' is cured<br/>';
+    }
+    function displayPre()
+    {
+        usort($this->pre, function($first,$second){
+            return $first->number > $second->number;});
+        foreach($this->pre as $key => $value){
+            echo "$key: $value<br/>";
+        }
+        // echo implode(' , ', $this->pre);
+    }
+    // function cmp($a, $b)
+    // {
+    //     if ($a == $b) {
+    //         return 0;
+    //     }
+    //     return ($a < $b) ? -1 : 1;
+    // }
+}
+$prexam = new PreExam();
+$prexam->addPatient('Smith', 5);
+$prexam->addPatient('Jones', 4);
+$prexam->addPatient('Fehrenbach', 6);
+$prexam->addPatient('Brown', 1);
+$prexam->addPatient('Ingram', 1);
+$prexam->curePatient();
+$prexam->displayPre();
+    // $prexam->sortPre();
+    // $prexam->displayPre();
