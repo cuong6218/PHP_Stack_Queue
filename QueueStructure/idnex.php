c0a824515711ee927429378a124319efa9e17353
cuong6218 <cuong6218@domain.com>
2020-07-02 00:19:01 +0700

    update file

--- QueueStructure/idnex.php
+++ QueueStructure/idnex.php
@@ -1,10 +1,13 @@
 <?php
-    // include_once ('Queue.php');
-    // $que1 = new Queue();
-    // $que1->enQueue(1);
-    // $que1->enQueue(2);
-    // $que1->enQueue(3);
-    // $que1->enQueue(4);
-    // echo "<pre>";
-    // print_r ($que1->display());
-    // echo "</pre>";
+include_once ('Queue.php');
+$que1 = new Queue();
+$que1->enQueue('Start');
+$que1->enQueue(1);
+$que1->enQueue(2);
+$que1->enQueue(3);
+$que1->enQueue(4);
+$que1->enQueue('End');
+$que1->enQueue('5');
+$que1->deQueue();
+
+echo implode('--', $que1->display());
